09bc390dae4cb2835c492a0f3bfd606470541641
Gonzalo Uceda <gonzalo.uceda@devo.com>
2022-03-02 15:54:40 +0100

    add linter

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -6,46 +6,46 @@ const REM_BASE = 16;
 const rem: Rem = (number: number) => number * REM_BASE;
 
 const theme: ThemeBase = {
-    spacing: {
-        rem,
-        sizes: {
-            xxsmall: rem(0.25),
-            xsmall: rem(0.5),
-            small: rem(0.75),
-            medium: rem(1),
-            large: rem(1.5),
-            xlarge: rem(2),
-            xxlarge: rem(3),
-        },
+  spacing: {
+    rem,
+    sizes: {
+      xxsmall: rem(0.25),
+      xsmall: rem(0.5),
+      small: rem(0.75),
+      medium: rem(1),
+      large: rem(1.5),
+      xlarge: rem(2),
+      xxlarge: rem(3),
     },
-    fonts: {
-        rem,
-        sizes: {
-            xxsmall: rem(0.75),
-            xsmall: rem(1),
-            small: rem(1.25),
-            medium: rem(1.5),
-            large: rem(2),
-            xlarge: rem(2.5),
-            xxlarge: rem(3.5),
-        },
+  },
+  fonts: {
+    rem,
+    sizes: {
+      xxsmall: rem(0.75),
+      xsmall: rem(1),
+      small: rem(1.25),
+      medium: rem(1.5),
+      large: rem(2),
+      xlarge: rem(2.5),
+      xxlarge: rem(3.5),
     },
-    shape: {
-        borderRadius: {
-            xxsmall: 2,
-            xsmall: 4,
-            small: 6,
-            medium: 8,
-            large: 12,
-            xlarge: 16,
-            xxlarge: 24,
-        },
+  },
+  shape: {
+    borderRadius: {
+      xxsmall: 2,
+      xsmall: 4,
+      small: 6,
+      medium: 8,
+      large: 12,
+      xlarge: 16,
+      xxlarge: 24,
     },
-    colors,
-    palette: {
-        white,
-        black,
-    },
+  },
+  colors,
+  palette: {
+    white,
+    black,
+  },
 };
 
 export default theme;
--- src/theme/types.ts
+++ src/theme/types.ts
@@ -2,68 +2,68 @@ export type Color = string;
 export type Rem = (number: number) => number;
 
 export interface ColorRange {
-    50: Color;
-    100: Color;
-    200: Color;
-    300: Color;
-    400: Color;
-    500: Color;
-    600: Color;
-    700: Color;
-    800: Color;
-    900: Color;
-    1000: Color;
-};
+50: Color;
+	100: Color;
+	200: Color;
+	300: Color;
+	400: Color;
+	500: Color;
+	600: Color;
+	700: Color;
+	800: Color;
+	900: Color;
+	1000: Color;
+}
 
 
 export interface Sizes {
-    xxsmall: number;
-    xsmall: number;
-    small: number;
-    medium: number;
-    large: number;
-    xlarge: number;
-    xxlarge: number;
-};
+xxsmall: number;
+	xsmall: number;
+	small: number;
+	medium: number;
+	large: number;
+	xlarge: number;
+	xxlarge: number;
+}
 
 export interface PaletteColor {
-    highlight: Color;
-    main: Color;
-    lowlight: Color;
-    contrast: Color;
+	highlight: Color;
+	main: Color;
+	lowlight: Color;
+	contrast: Color;
 }
 
 export interface ThemeBase {
-    spacing: {
-        rem: Rem;
-        sizes: Sizes;
-    }
-    fonts: {
-        rem: Rem;
-        sizes: Sizes;
-    }
-    shape: {
-        borderRadius: Sizes;
-    }
-    colors: {
-        [key: string]: ColorRange;
-    }
-    palette: {
-        white: Color;
-        black: Color;
-    }
-};
+	spacing: {
+		rem: Rem;
+		sizes: Sizes;
+	}
+	fonts: {
+		rem: Rem;
+		sizes: Sizes;
+	}
+	shape: {
+		borderRadius: Sizes;
+	}
+	colors: {
+		[key: string]: ColorRange;
+	}
+	palette: {
+		white: Color;
+		black: Color;
+	}
+}
 
 
 export interface Theme extends ThemeBase {
-    palette: {
-        white: Color;
-        black: Color;
-        primary: PaletteColor;
-        secondary: PaletteColor;
-        error: PaletteColor;
-        success: PaletteColor;
-        alpha: ColorRange;
-        text: Color;
-    }
+	palette: {
+		white: Color;
+		black: Color;
+		primary: PaletteColor;
+		secondary: PaletteColor;
+		error: PaletteColor;
+		success: PaletteColor;
+		alpha: ColorRange;
+		text: Color;
+	}
 }
