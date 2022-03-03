e271e3c274b72e185c0c95d68ce9a28ca8022efb
Gonzalo Uceda <gonzalo.uceda@devo.com>
2022-03-03 18:43:07 +0100

    remove warning

--- src/App.tsx
+++ src/App.tsx
@@ -21,32 +21,32 @@ const entries = [
     description:'This is the first command that you will ever see in your life. It is beautiful and at the same time interesting as f**k'
   },
   {
-    command:'command0',
+    command:'command3',
     params:['param1', 'param2'],
     description:'This is the first command that you will ever see in your life. It is beautiful and at the same time interesting as f**k'
   },
   {
-    command:'command1',
+    command:'command4',
     params:['param1'],
     description:'This is the first command that you will ever see in your life. It is beautiful and at the same time interesting as f**k'
   },
   {
-    command:'command2',
+    command:'command5',
     params:['param1', 'param2'],
     description:'This is the first command that you will ever see in your life. It is beautiful and at the same time interesting as f**k'
   },
   {
-    command:'command0',
+    command:'command6',
     params:['param1', 'param2'],
     description:'This is the first command that you will ever see in your life. It is beautiful and at the same time interesting as f**k'
   },
   {
-    command:'command1',
+    command:'command7',
     params:['param1'],
     description:'This is the first command that you will ever see in your life. It is beautiful and at the same time interesting as f**k'
   },
   {
-    command:'command2',
+    command:'command8',
     params:['param1', 'param2'],
     description:'This is the first command that you will ever see in your life. It is beautiful and at the same time interesting as f**k'
   },
