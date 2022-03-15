150d83c33604f68115c06b73e5b634bf177e61df
Gonzalo Uceda <gonzalo.uceda@devo.com>
2022-03-15 12:01:14 +0100

    clean UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import entries from './mock/entries';
 import './App.css';
 import Layout from './components/atoms/layout/Layout';
 import Divider from './components/atoms/divider/Divider';
-import Text from './components/atoms/text/Text';
 import SemanticSearchService from './services/SemanticSearchService';
 import List from './components/molecules/list/List';
 
@@ -28,15 +27,6 @@ const App: FC = () => {
             <Divider />
             <CodeEditor content="const test = null;" />
           </Layout>
-          <Text.H1 color="white">H1</Text.H1>
-          <Text.H2 color="white">H2</Text.H2>
-          <Text.H3 color="white">H3</Text.H3>
-          <Text.H4 color="white">H4</Text.H4>
-          <Text.H5 color="white">H5</Text.H5>
-          <Text.H6 color="white">H6</Text.H6>
-          <Text.Copy color="white">Copy</Text.Copy>
-          <Text.CopySmall color="white">CopySmall</Text.CopySmall>
-          <Text.Label color="white">Label</Text.Label>
         </header>
       </div>
     </ThemeProvider>
