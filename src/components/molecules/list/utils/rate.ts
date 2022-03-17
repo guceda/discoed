e2d8a239cb8c14de6d3b37abadc0d63ea4a03ba1
Gonzalo Uceda <gonzalo.uceda@devo.com>
2022-03-17 12:34:54 +0100

    add rating

--- src/components/molecules/list/utils/rate.ts
+++ src/components/molecules/list/utils/rate.ts
@@ -4,6 +4,22 @@ export interface RatedEntry extends HighLightedEntry {
   score: number;
 }
 
+const MAX_SCORE = 100;
+
 export default (entries: HighLightedEntry[]): RatedEntry[] => {
-  return entries.map((entry) => ({ ...entry, score: 0 }));
+  return entries.map((entry) => {
+    let score = 0;
+
+    entry.highlights.forEach((match) => {
+      if (match.property === 'description') score += 1;
+
+      if (match.property === 'command') {
+        const matchLength = match.end - match.start;
+        const leftOutCharacters = entry.command.length - matchLength;
+        score += MAX_SCORE - leftOutCharacters;
+      }
+    });
+
+    return { ...entry, score: score / 100 };
+  });
 };
