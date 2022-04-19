9feda630290a61a2acd2bf272062586b68f62b63
Gonzalo Uceda <gonzalo.uceda@devo.com>
2022-04-19 16:17:57 +0200

    select text

--- src/hooks/useFocus.tsx
+++ src/hooks/useFocus.tsx
@@ -3,7 +3,10 @@ import { useCallback, useRef } from 'react';
 const useFocus = () => {
   const htmlElRef = useRef<any>(null);
   const setFocus = useCallback(() => {
-    if (htmlElRef.current) htmlElRef.current.focus();
+    if (htmlElRef.current) {
+      htmlElRef.current.focus();
+      htmlElRef.current.select();
+    }
   }, [htmlElRef]);
 
   return [htmlElRef, setFocus];
